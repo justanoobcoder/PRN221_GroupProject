b6402c12cd9320d1f88e31306493147fa7bde1f5
HaiHa01 <147325937+HaiHa01@users.noreply.github.com>
2023-10-29 12:55:29 +0700

    them_Register_Details_Update

--- LaundryMiddlePlatform/RazorPages/Program.cs
+++ LaundryMiddlePlatform/RazorPages/Program.cs
@@ -5,6 +5,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddRazorPages().AddNToastNotifyNoty(new NToastNotify.NotyOptions()
+{
+    ProgressBar = true,
+    Timeout = 2000
+});
 builder.Services.AddSession(options =>
 {
     options.IdleTimeout = TimeSpan.FromMinutes(30);
@@ -32,6 +37,8 @@ app.UseSession();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.UseNToastNotify();
 
 app.MapRazorPages();
 
