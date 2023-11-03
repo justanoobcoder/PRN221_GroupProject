fd52fee4b4a8aa0e2592d6065385df47140f11ca
HaiHa01 <147325937+HaiHa01@users.noreply.github.com>
2023-11-03 14:49:12 +0700

    sua_GiaoDien_Service_DetailsService

---
+++ LaundryMiddlePlatform/RazorPages/Pages/Store/ServiceManager/DetailsService.cshtml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BusinessObjects;
+using Repositories;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace RazorPages.Pages.StoreNamespace
+{
+    public class DetailsServiceModel : PageModel
+    {
+        private readonly IServiceRepository _iServiceRepository;
+        private readonly IStoreRepository _iStoreRepository;
+
+
+        public DetailsServiceModel(IServiceRepository iServiceRepository, IStoreRepository iStoreRepository)
+        {
+            _iServiceRepository = iServiceRepository;
+            _iStoreRepository = iStoreRepository;
+        }
+
+        public Service Service { get; set; } = default!;
+        public List<SelectListItem> StoresSelectList { get; set; } = default!;
+        public BusinessObjects.Store Store { get; set; } = default!;
+
+
+        public IActionResult OnGet(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+            Service = _iServiceRepository.GetById(id);
+            if (Service == null)
+            {
+                return NotFound();
+            }
+                //var storesData = _iStoreRepository.GetStoresAvailable();
+                var storeData = _iStoreRepository.GetStores();
+                Store = storeData.Where(s => s.Services.Where(s => s.Id == id).FirstOrDefault() != null).FirstOrDefault();
+               
+            return Page();
+        }
+
+    }
+}
